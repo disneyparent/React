37ca08e08a0d3c393d601388d58626f1fb620991
abqkatrina <abqkatrina@gmail.com>
2020-03-02 22:45:49 -0700

    working on Welcome page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';import './App.css';
 
 function App() {
+
   return (
-    <></>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Login} />
+        <PrivateRoute exact path='/welcome' component={Welcome} />
+      </div>
+    </Router>
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,66 +13,75 @@ export const initialState = {
 
 export const Reducer = (state = initialState, action) => {
 
-switch (action.type) {
+    switch (action.type) {
 
-    case 'GET_USER_START':
-        return{
-            ...state,
-            isLoading: true
-        }
+        case 'GET_USER_START':
+            return{
+                ...state,
+                isLoading: true
+            }
 
-    case 'GET_USER_WIN':
-        return{
-            ...state,
-            users: action.payload,
-            isLoading: false
-        }
+        case 'GET_USER_WIN':
+            return{
+                ...state,
+                users: action.payload,
+                isLoading: false
+            }
 
-    case 'GET_USER_LOSE':
-        return{
-            ...state,
-            isLoading: false
-        }	
+        case 'GET_USER_LOSE':
+            return{
+                ...state,
+                isLoading: false
+            }	
 
-    case 'ADD_USER':
-        return {
-            ...state,
-            users: [ ...state.users, action.payload],
-            isLoading: false
-        }
-    
+        case 'ADD_USER':
+            return {
+                ...state,
+                users: [ ...state.users, action.payload],
+                isLoading: false
+            }
+        
         case 'GET_BUGGY_WIN':
             return{
                 ...state,
                 buggies: action.payload,
                 isLoading: false
             }
-    
+
         case 'GET_BUGGY_LOSE':
             return{
                 ...state,
                 isLoading: false
             }	
-    
+        
         case 'ADD_BUGGY':
+                return {
+                    ...state,
+                    buggies: [ ...state.buggies, action.payload],
+                    isLoading: false
+                }
+
+        case 'DELETE_BUGGY':
             return {
                 ...state,
-                buggies: [ ...state.buggies, action.payload],
+                buggies: [
+                    ...state.buggies.filter( buggy =>
+                    buggy.id !== action.payload.id )
+                ]
+            }        
+
+        case 'DELETE_USER':
+            return {
+                ...state,
+                users: [
+                    ...state.users.filter( user => 
+                    user.id !== action.payload.id )	
+                ],
                 isLoading: false
             }
 
-    case 'DELETE_USER':
-        return {
-            ...state,
-            users: [
-                ...state.users.filter( user => 
-                user.id !== action.payload.id)	
-            ],
-            isLoading: false
-        }
 
-
-    default:
-        return state;
-}
+        default:
+            return state;
+    }
 };
