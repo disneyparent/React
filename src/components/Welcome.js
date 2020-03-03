7de6a70f2baae8d17eb58c3100ac9bf71b525446
Tyler Kummer <tylerkummer@protonmail.com>
2020-03-03 14:36:23 -0600

    Welcome Form

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -39,30 +39,59 @@ const Welcome = () => {
 // }
 
     return(
-        // <div className='card'>
-        //     <h3></h3>
-        //     <button >I need to borrow a stroller.</button>
-                 
-        //         <form className='hiddenForm'>
-        //             <input type='checkbox'>single</input>
-        //             <input type='checkbox'>double</input>
-        //             <button >Borrow</button>
-        //         </form>
-        //         }
-        //     <button >I want to leave my stroller.</button>
-                
-        //             <form className='hiddenForm'>
-        //                 <input type='checkbox'>single</input>
-        //                 <input type='checkbox'>double</input>
-        //                 <button >Share</button>
-        //             </form>
-        //         }
-        //     <button >De-commission borken buggy</button>    
-        //     <button >Delete My Account</button>
-        // </div>
-
         <div>
-            
+            <form>
+                <h2>I Need to Borrow a Stroller</h2>
+                <input type="checkbox"
+                    name="single"
+                    checked="true"
+                    //value={}
+                    //onChange={e => }
+                />
+                <label for="single">Single Cart</label>
+
+                <input type ="checkbox" 
+                    name="double"
+                    //value={}
+                    //onChange={e => }
+                />
+                <label for="double">Double Cart</label>
+
+                <button>Submit</button>
+            </form>
+
+            <form>
+                <h2>I Want to Share My Stroller</h2>
+                <input type="checkbox"
+                    name="single"
+                    checked="true"
+                    //value={}
+                    //onChange={e => }
+                />
+                <label for="single">Single Cart</label>
+
+                <input type ="checkbox" 
+                    name="double"
+                    //value={}
+                    //onChange={e => }
+                />
+                <label for="double">Double Cart</label>
+
+                <select>
+                    <option>Chose a Location</option>
+                    <option>Jungle Cruise</option>
+                    <option>Big Thunder Mountain</option>
+                    <option>Splash Mountain</option>
+                    <option>Smuggler's Run</option>
+                    <option>Minnie's House</option>
+                    <option>It's a Small World</option>
+                    <option>Peter Pan's Flight</option>
+                    <option>Space Mountain</option>
+                </select>
+
+                <button>Submit</button>
+            </form>
+
         </div>
     )
 }
